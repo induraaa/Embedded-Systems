28cfcc7fd5d6a62245f67f9f395425af0f610ef8
induraaa <129575423+induraaa@users.noreply.github.com>
2023-10-26 15:56:46 +0100

    130

--- Tasks/Task-126-DigitalIn-Solution/main.cpp
+++ Tasks/Task-126-DigitalIn-Solution/main.cpp
@@ -1,14 +1,15 @@
 #include "mbed.h"
 
 DigitalIn ButtonA(PG_0); //Button A
-DigitalIn ButtonB(PG_1); //Button B
+DigitalIn ButtonB(PG_1); //Button A
+DigitalIn ButtonC(PG_2, PinMode::PullDown); //Button C
+DigitalIn ButtonD(PG_3, PinMode::PullDown); //Button D
 
 DigitalOut redLED(PC_2); //Red Traffic 1
-
 // main() runs in its own thread in the OS
 int main()
 {
-    int btnA, btnB;
+    int btnA, btnB, btnC, btnD;
     // Turn OFF the red LED
     redLED = 0;
 
@@ -18,11 +19,12 @@ int main()
         do {
             btnA = ButtonA; //Read button A
             btnB = ButtonB; //Read button B
-        } while ((btnA == 0) || (btnB == 0));
+            btnC = ButtonC;
+            btnC = ButtonD;
+        } while ((btnA == 0) || (btnB == 0) || (btnC==0) || (btnD==0));
 
         //Toggle the red LED
         redLED = !redLED;
-
         //Wait for noise to settle
         wait_us(10000);
 
@@ -30,7 +32,9 @@ int main()
         do {
             btnA = ButtonA; //Read button A
             btnB = ButtonB; //Read button B
-        } while ((btnA == 1) && (btnB == 1));
+            btnC = ButtonC;
+            btnD = ButtonD;
+        } while ((btnA == 1) && (btnB == 1) && (btnC == 1) && (btnD == 1));
 
         //Wait for noise to settle
         wait_us(10000);
