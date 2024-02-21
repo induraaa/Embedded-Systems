f2d1189c0fce28d19e5bb0e4d42f687c07c2d4f4
induraaa <129575423+induraaa@users.noreply.github.com>
2024-02-21 09:39:10 +0000

    mm

--- Tasks/Task-115-BusOut/main.cpp
+++ Tasks/Task-115-BusOut/main.cpp
@@ -2,9 +2,9 @@
 #define WAIT_TIME_MS 1000 
 
 
-// DigitalOut redLED(PC_2,1);
-// DigitalOut yellowLED(PC_3,1);
-// DigitalOut greenLED(PC_6,1);
+DigitalOut redLED(PC_2,1);
+DigitalOut yellowLED(PC_3,1);
+DigitalOut greenLED(PC_6,1);
 
 BusOut lights(PC_2, PC_3, PC_6, PB_0, PB_7, PB_14);
 
@@ -17,12 +17,12 @@ int main()
 
     while (true)
     {
-        int count = 0b111111;
+        int count = 0b111;
         while (count >= 0) 
         {
             printf("count = %d\n", count);
             lights = count;
-            wait_us(100000);
+            wait_us(1000000);
 
             count = count - 1;
         }
